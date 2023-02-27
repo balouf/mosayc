d3e66972adefec42de4e2541314342913194b0d8
balouf <fabien.mathieu@normalesup.org>
2023-02-27 10:01:29 +0000

    Deploying to gh-pages from @ balouf/mosayc@87b08db4bfc0d9773be2efdaf9c3901047623580 🚀

--- lib/mosayc/__init__.py
+++ lib/mosayc/__init__.py
@@ -5,6 +5,8 @@ __email__ = 'fabien.mathieu@normalesup.org'
 __version__ = '0.1.0'
 
 
+from mosayc.config.loader import load_cfg
+
 from mosayc.sub_package_1.my_class_1 import MyClass1
 from mosayc.sub_package_2.my_class_2 import MyClass2
 from mosayc.sub_package_2.my_class_3 import MyClass3
